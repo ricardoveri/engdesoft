f36acbad241b370f9fdce16cc9f66a9c4d692342
ricardoveri <ricardoverissimo1000@gmail.com>
2022-05-24 20:03:39 -0300

    é isso aí

---
+++ src/Juliana/Matematica.java
@@ -0,0 +1,13 @@
+package Juliana;
+
+public class Matematica {
+    int numero1 = 10;
+    int numero2 = 20;
+    static int soma;
+
+    public static int soma(int numero1, int numero2){
+        return soma = numero1 + numero2;
+    }
+
+}
+
